ffe50f38dd44e7b90315e03deb58103dcaf635d9
Aria <36065920+Fatin-Almalik@users.noreply.github.com>
2022-10-11 16:36:14 +0300

    Add scores to the game, and the ability to win or lose the game

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 
-rock.addEventListener('click', function(params) {gameWithUI('rock')});
-paper.addEventListener('click', function(params) {gameWithUI('paper')});
-scissors.addEventListener('click', function(params) {gameWithUI('scissors')});
+//add a button to reset playerScore and computerScore? To reset the game.
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener('click', function() {gameWithUI('rock')});
+paper.addEventListener('click', function() {gameWithUI('paper')});
+scissors.addEventListener('click', function() {gameWithUI('scissors')});
 
 const roundText = document.querySelector('.round');
+const winnerText = document.querySelector('.winner_announcement');
+const scoreText = document.querySelector('.scores');
+
 
 function gameWithUI(playerPick) {
     let computerChoice = getComputerChoice()
     result = playRound(playerPick, computerChoice);
     roundText.textContent = `Computer picked ${computerChoice}, you picked ${playerPick}. ${result}`;
+    
+    keepingScores(result); //this is not written well, keepingScores() does more than just keeping scores.. refine this
+    scoreText.textContent = `You: ${playerScore}, computer: ${computerScore}`;
+    
 }
 
+function keepingScores(roundResult) {
+    if (roundResult == 'You won!') {
+        playerScore++;
+    } else if (roundResult == 'You lost.') {
+        computerScore++;
+    }
+    if (playerScore === 5) {
+        winnerText.textContent = "You've won 5 rounds! You won this game!"
+    }
+    if (computerScore === 5) {
+        winnerText.textContent = "The computer won 5 rounds! You lost this game!"
+    }
+}
 
 function getComputerChoice() {
     const list = ['rock', 'paper', 'scissors']
@@ -45,35 +69,3 @@ function playRound(playerSelection, computerSelection) {
         return "You lost.";
     }
 }
-
-
-
-function game(rounds) {
-    //get user input using prompt()
-    //print results of that round
-    //repeat, new round, new input, new result
-    //once all rounds done, print score, computer = x wins, user = y wins, computer/user won.
-
-    let playerScore = 0;
-
-    for (let i = 0; i < rounds; i++) {
-        let playerSelection = prompt("Rock, paper, or scissors?");
-        // uncomment this once checkPlayerInput() is fixed.
-        // playerSelection = checkPlayerInput(playerSelection);
-
-        const computerSelection = getComputerChoice();
-        let roundResult = playRound(playerSelection, computerSelection);
-        
-        console.log(`Computer chose ${computerSelection}. ${roundResult}`);
-
-        if (roundResult == 'You won!') {
-            playerScore++;
-        }
-        
-    }
-    console.log(`Your score: you won ${playerScore} times out of ${rounds} rounds.`)
-}
-
-
-
-// game(5);
